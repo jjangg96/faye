6fe9e31042ae5574cccf818770205e7fac34603c
jjangg96 <jjangg96@gmail.com>
2015-07-29 15:49:28 +0900

    add check, see crontab

---
+++ check.js
@@ -0,0 +1,31 @@
+var request = require("request");
+
+function restart() {
+  var spawn = require('child_process').spawn;
+  var child = spawn('sudo', ['service', 'korea', 'restart']);
+}
+
+//Error catch
+process.on('uncaughtException', function (err) {
+  console.log('[' + new Date() + '][Error] ' + err.stack);
+}); 
+
+
+
+request({uri: 'http://localhost:8888/timestamp', timeout: 5000 }, function(err, response, body) {
+  if(err || response.statusCode != 200) {
+    console.log('Server down');
+    restart();
+  } else if(response.statusCode == 200) {
+    if(body != "") {
+      var delay = parseInt(body);  
+
+      if(delay > 10*60*1000) {
+        restart();
+      } else {
+        console.log(delay);
+      }
+    }
+  }
+});  
+
